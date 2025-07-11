40c41f910771758fa49501ca910563e4103a06da
BlueOrgreen <1936341390@qq.com>
2025-07-11 18:51:45 +0800

    feat: 上传Media模块开发

--- src/modules/media/controllers/media.controller.ts
+++ src/modules/media/controllers/media.controller.ts
@@ -11,19 +11,6 @@ import { NotEmptyPipe } from '@/modules/core/pipes/not-empty.pipe';
 export class MediaController {
     constructor(protected service: MediaService) {}
 
-    getMimeType(ext: string): string {
-        const mimeMap = {
-            '.png': 'image/png',
-            '.jpg': 'image/jpeg',
-            '.jpeg': 'image/jpeg',
-            '.gif': 'image/gif',
-            '.webp': 'image/webp',
-            '.svg': 'image/svg+xml',
-        };
-        // @ts-ignore
-        return mimeMap[ext.toLowerCase()] || 'application/octet-stream';
-    }
-
     @Get('images/:id.:ext')
     @Guest()
     async image(
@@ -32,8 +19,6 @@ export class MediaController {
         @Res({ passthrough: true }) res: FastifyReply,
     ) {
         const result = await this.service.loadImage(id, res, `.${ext}`);
-        console.log('result===>', result);
-
         return res.send(result);
     }
 }
